276fdec0a1690e65d8e89219e0ad61dfa7b3dc54
kivancakmak <kivanccakmak35@gmail.com>
2026-05-21 15:20:35 +0300

    Section 8 and TASK 1 is done

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         page = GeneratorPage();
@@ -167,5 +167,31 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     });
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('No favorites yet.'));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('You have ${appState.favorites.length} favorites:'),
+        ),
+        ...appState.favorites.map((pair) => ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text(pair.asLowerCase),
+            )),
+      ],
+    );
   }
 }
